9c3513b44d6e2fe82974d2c84a5ba20f7ad36904
Zibi Braniecki <zibi@braniecki.net>
2021-01-18 14:39:35 -0800

    Iterator revision

--- utils/simple_formatter/benches/parser.rs
+++ utils/simple_formatter/benches/parser.rs
@@ -7,28 +7,34 @@ use icu_simple_formatter::*;
 
 fn parser(c: &mut Criterion) {
     let samples = vec![
-        ("Foo {0} and {1}", vec!["Hello", "World"]),
-        ("Foo {1} and {0}", vec!["Hello", "World"]),
-        ("{start}, {midde} and {end}", vec!["Start", "Middle", "End"]),
-        // ("{0} 'at' {1}", vec!["Hello", "World"]),
+        ("Foo {0} and {1}", vec![vec!["Hello"], vec!["World"]]),
+        (
+            "{0}, {1} and {2}",
+            vec![vec!["Start"], vec!["Middle"], vec!["End"]],
+        ),
+        // ("{start}, {midde} and {end}", vec!["Start", "Middle", "End"]),
+        ("{0} 'at' {1}", vec![vec!["Hello"], vec!["World"]]),
     ];
 
     c.bench_function("parser/idx", |b| {
         b.iter(|| {
             for sample in &samples {
-                let parser = Parser::new();
-                let _ = parser.parse(sample.0);
+                let _ = parse::<usize>(sample.0).count();
             }
         })
     });
 
-    c.bench_function("format/idx", |b| {
+    c.bench_function("interpolate/idx", |b| {
         b.iter(|| {
             for sample in &samples {
-                let parser = Parser::new();
-                let elements = parser.parse(sample.0);
-                let mut result = String::new();
-                write_format(&mut result, &elements, &sample.1);
+                let iter = parse(sample.0);
+
+                let replacements: Vec<Vec<Token>> = sample
+                    .1
+                    .iter()
+                    .map(|r| r.iter().map(|t| Token::Literal(t)).collect())
+                    .collect();
+                let i = interpolate(iter, replacements).count();
             }
         })
     });
--- utils/simple_formatter/src/lib.rs
+++ utils/simple_formatter/src/lib.rs
@@ -1,149 +1,160 @@
-#[derive(Debug, PartialEq)]
-pub struct Literal<'s> {
-    value: &'s str,
-    quotes: Vec<usize>,
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug)]
+pub enum Element<'s, P>
+where
+    P: FromStr,
+{
+    Placeholder(P),
+    Literal(&'s str),
 }
 
-impl<'s> Literal<'s> {
-    pub fn new(value: &'s str) -> Self {
-        Self {
-            value,
-            quotes: vec![],
-        }
-    }
-}
+pub fn parse<P>(input: &str) -> impl Iterator<Item = Element<'_, P>> + '_
+where
+    P: FromStr,
+    <P as FromStr>::Err: std::fmt::Debug,
+{
+    let mut chars = input.chars().enumerate();
+    let mut start_idx = 0;
+    let mut in_placeholder = false;
 
-#[derive(Debug, PartialEq)]
-pub enum Element<'s> {
-    Placeholder(&'s str),
-    Literal(Literal<'s>),
-}
-
-pub struct Parser {}
-
-impl Parser {
-    pub fn new() -> Self {
-        Self { }
-    }
-
-    pub fn parse<'l>(&self, input: &'l str) -> Vec<Element<'l>> {
-        let mut result = vec![];
-        let mut in_placeholder = false;
-        let mut quotes = vec![];
-        let mut start = 0;
-
-        let mut bytes = input.bytes().enumerate();
-        while let Some((idx, b)) = bytes.next() {
+    std::iter::from_fn(move || {
+        while let Some((idx, ch)) = chars.next() {
             if in_placeholder {
-                if b == b'}' {
-                    assert!(idx != start);
-                    result.push(Element::Placeholder(&input[start..idx]));
-                    in_placeholder = false;
-                    start = idx + 1;
-                }
-            } else {
-                if b == b'{' {
-                    if start != idx {
-                        result.push(Element::Literal(Literal {
-                            value: &input[start..idx],
-                            quotes
-                        }));
-                        quotes = vec![];
-                    }
-                    in_placeholder = true;
-                    start = idx + 1;
-                } else if b == b'\'' {
-                    quotes.push(idx - start);
-                    bytes.next();
+                let ret = &input[idx..idx + 1];
+                chars.next();
+                in_placeholder = false;
+                start_idx = idx + 2;
+                return Some(Element::Placeholder(ret.parse().unwrap()));
+            }
+            if ch == '{' {
+                in_placeholder = true;
+                if start_idx != idx {
+                    let r: &str = &input[start_idx..idx];
+                    let ret = Some(Element::Literal(r));
+                    return ret;
+                } else {
+                    continue;
                 }
             }
         }
-        result
+        None
+    })
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum Token<'s> {
+    Literal(&'s str),
+}
+
+impl<'s> std::fmt::Display for Token<'s> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Literal(s) => f.write_str(s),
+        }
     }
 }
 
-use std::fmt::{Error, Write};
+pub trait ReplacementProvider<'s, H>
+where
+    H: FromStr,
+{
+    type Iter: Iterator<Item = Token<'s>>;
 
-//XXX: We need a way to produce an interpolated list of elements, rather than a string
-pub fn write_format<W: Write>(f: &mut W, pattern: &[Element], replacements: &[&str]) -> Result<(), Error> {
-    for element in pattern {
-        match element {
-            Element::Literal(s) => {
-                if s.quotes.is_empty() {
-                    f.write_str(s.value)?;
-                } else {
-                    let mut start = 0;
-                    for idx in &s.quotes {
-                        if start != *idx {
-                            f.write_str(&s.value[start..*idx])?;
-                        }
-                        start = idx + 1;
-                    }
-                    if start < s.value.len() {
-                        f.write_str(&s.value[start..])?;
-                    }
+    fn get_replacement(&self, input: H) -> Self::Iter;
+}
+
+impl<'s> ReplacementProvider<'s, usize> for Vec<Vec<Token<'s>>> {
+    type Iter = <Vec<Token<'s>> as IntoIterator>::IntoIter;
+
+    fn get_replacement(&self, input: usize) -> Self::Iter {
+        self.get(input).unwrap().clone().into_iter()
+    }
+}
+
+pub fn interpolate<'s, P, F, H>(mut pattern: P, replacements: F) -> impl Iterator<Item = Token<'s>>
+where
+    P: Iterator<Item = Element<'s, H>>,
+    H: FromStr,
+    F: ReplacementProvider<'s, H>,
+{
+    let mut current_placeholder: Option<F::Iter> = None;
+
+    std::iter::from_fn(move || {
+        if let Some(ref mut placeholder) = &mut current_placeholder {
+            if let Some(v) = placeholder.next() {
+                return Some(v);
+            } else {
+                current_placeholder = None;
+            }
+        }
+        if let Some(element) = pattern.next() {
+            match element {
+                Element::Literal(v) => {
+                    return Some(Token::Literal(v));
                 }
-            },
-            Element::Placeholder(s) => {
-                f.write_str(s)?;
-            },
-        };
-    }
-    Ok(())
-}
-
-struct Serializer {}
-
-impl Serializer {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn serialize(&self, input: Vec<Element>) -> String {
-        String::new()
-    }
+                Element::Placeholder(p) => {
+                    let mut r = replacements.get_replacement(p);
+                    let ret = r.next();
+                    current_placeholder = Some(r);
+                    return ret;
+                }
+            }
+        } else {
+            return None;
+        }
+    })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // Foo {0} and {1}
-    // Foo {1} and {0}
-    // DD/MM/yy 'at' HH:mm
-    // yyyy.MM.dd G 'at' HH:mm:ss zzz
-    // EEE, MMM d, ''yy
+    const samples: &[(&str, &[&[&str]], &str)] = &[
+        (
+            "Foo {0} and {1}",
+            &[&["Hello"], &["World"]],
+            "Foo Hello and World",
+        ),
+        (
+            "{0}, {1} and {2}",
+            &[&["Start"], &["Middle"], &["End"]],
+            "Start, Middle and End",
+        ),
+        (
+            "{0} 'at' {1}",
+            &[&["Hello"], &["World"]],
+            "Hello 'at' World",
+        ),
+    ];
+
     #[test]
-    fn it_works() {
-        let parser = Parser::new();
-        let ast = parser.parse("Foo {0} and {1}");
-        assert_eq!(ast, vec![
-            Element::Literal(Literal { value: "Foo ", quotes: vec![] }),
-            Element::Placeholder("0"),
-            Element::Literal(Literal { value: " and ", quotes: vec![] }),
-            Element::Placeholder("1"),
-        ]);
+    fn simple_parse() {
+        let iter = parse("{0} and {1}");
+        let v: Vec<Element<usize>> = iter.collect();
+        assert_eq!(
+            v,
+            vec![
+                Element::Placeholder(0),
+                Element::Literal(" and "),
+                Element::Placeholder(1),
+            ]
+        );
+    }
 
-        let parser = Parser::new();
-        let ast = parser.parse("{start}, {middle} and {end}");
-        assert_eq!(ast, vec![
-            Element::Placeholder("start"),
-            Element::Literal(Literal { value: ", ", quotes: vec![] }),
-            Element::Placeholder("middle"),
-            Element::Literal(Literal { value: " and ", quotes: vec![] }),
-            Element::Placeholder("end"),
-        ]);
+    #[test]
+    fn simple_interpolate() {
+        for sample in samples.iter() {
+            let iter = parse(sample.0);
 
-        let parser = Parser::new();
-        let ast = parser.parse("{0} 'at' {1}");
-        let mut s = String::new();
-        write_format(&mut s, &ast, &["Hello", "World"]);
-        assert_eq!(s, "0 at 1");
-
-        assert_eq!(ast, vec![
-            Element::Placeholder("0"),
-            Element::Literal(Literal { value: " 'at' ", quotes: vec![1, 4] }),
-            Element::Placeholder("1"),
-        ]);
+            let replacements: Vec<Vec<Token>> = sample
+                .1
+                .iter()
+                .map(|r| r.iter().map(|t| Token::Literal(t)).collect())
+                .collect();
+            let i = interpolate(iter, replacements);
+            let result = i.map(|t| t.to_string()).collect::<Vec<_>>().join("");
+            assert_eq!(result, sample.2);
+        }
     }
 }
